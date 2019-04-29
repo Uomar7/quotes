756d15a4c779b4d567a999fe5497b83ba5fb8cf6
Uomar <uomarearlie7@gmail.com>
2019-04-29 10:55:19 +0300

    linked the class quote to the quote coomponent

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import {Quote} from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -7,6 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
  heading = 'Quotes'
+
+ quotes=[
+   
+ ]
+ toogleDetails(index){
+   this.quotes[index].showDescription = !this.quotes[index].showDescription;
+ }
+ addNewQuote(quote){
+   let quoteLength = this.quotes.length;
+   quote.id = quoteLength+1;
+   quote.postDate = new Date(quote.postDate)
+   this.quotes.push(quote)
+ }
+ deleteQuote(isComplete,index){
+   if (isComplete){
+     let toDelete = confirm(`are you sure you want to delete ${this.quotes[index].name}`)
+     if (toDelete){
+       this.quotes.splice(index,1)
+     }
+   }
+ }
   constructor() { }
 
   ngOnInit() {
